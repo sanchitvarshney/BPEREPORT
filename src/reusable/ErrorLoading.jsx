e7297baae9689700196bc90683c24a282a7549ab
sanchitvarshney <sanchit.varshney@mscorpres.in>
2025-03-10 09:53:28 +0530

    Add ErrorLoading handling

---
+++ src/reusable/ErrorLoading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorLoading = () => (
+  <div style={{ color: 'red', textAlign: 'center' }}>
+    <h2>Error loading the component. Please reload the page.</h2>
+  </div>
+);
+
+export default ErrorLoading;
