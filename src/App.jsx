dc3eae0e2fac78a078981337053176e1ade2e987
sanchitvarshney <sanchit.varshney@mscorpres.in>
2025-12-17 17:09:52 +0530

    Revert "added google login module"
    This reverts commit 0af66e7a1c13bf3f5fabea83cf8961aa836c5767.

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,8 @@ import { Provider } from 'react-redux';
 import { store } from 'features/store';
 import { ToastProvider } from 'utils/ToastProvider';
 import { SocketProvider } from 'contexts/SocketContext';
-import { GoogleOAuthProvider } from "@react-oauth/google";
-const googleId = import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID;
 export default function App() {
-
   return (
-    <GoogleOAuthProvider clientId={googleId}>
     <ThemeCustomization>
       <ToastProvider>
       <SocketProvider>
@@ -23,6 +19,5 @@ export default function App() {
         </SocketProvider>
       </ToastProvider>
     </ThemeCustomization>
-    </GoogleOAuthProvider>
   );
 }
